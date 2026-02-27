6e17a5757fe9efdc256859a4a4bc87abf0761a6b
chetu1993 <schetankumar123@gmail.com>
2026-02-27 15:13:42 +0530

    refractor: implemented helper function for get and put methods

--- app/main.py
+++ app/main.py
@@ -14,6 +14,11 @@ class Employee(BaseModel):
 class EmployeeResponse(Employee):
     employee_id:int
 
+def get_employee_or_404(employee_id:int):
+    employee=employees_db[employee_id]
+    if not employee:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Employee not found")
+    return employee
 
 @app.post("/employees",response_model=EmployeeResponse,status_code=status.HTTP_201_CREATED)
 def create_employee(employee:Employee):
@@ -25,16 +30,14 @@ def create_employee(employee:Employee):
 
 @app.get("/employees/{employee_id}",response_model=EmployeeResponse,status_code=status.HTTP_200_OK)
 def get_employee(employee_id:int):
-    if employee_id not in employees_db:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Employee not found")
-    employee=employees_db[employee_id]
+    employee=get_employee_or_404(employee_id)
+
     return EmployeeResponse(employee_id=employee_id,**employee.model_dump())
 
 @app.put("/employees/{employee_id}",response_model=EmployeeResponse,status_code=status.HTTP_200_OK)
 def update_employee(employee_id:int,updated_employee:Employee):
-    if employee_id not in employees_db:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Employee not found")
-    employees_db[employee_id]=updated_employee
+    get_employee_or_404(employee_id)
+    updated_employee=employees_db[employee_id]
     return EmployeeResponse(employee_id=employee_id,**updated_employee.model_dump())
 
 

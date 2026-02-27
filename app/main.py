ecb4acfa2b027bbf6029c5ef88d0643ff4b4a5f4
chetu1993 <schetankumar123@gmail.com>
2026-02-27 20:47:30 +0530

    feat: updated the put method

--- app/main.py
+++ app/main.py
@@ -48,23 +48,32 @@ def get_employee(employee_id:int):
                             country=row[3],
                             salary=row[4])
 
-@app.put("/employees/{employee_id}",response_model=EmployeeResponse,status_code=status.HTTP_200_OK)
+@app.put("/employees/{employee_id}",response_model=EmployeeResponse)
 def update_employee(employee_id:int,employee:Employee):
     conn=get_connection()
     cursor=conn.cursor()
+    cursor.execute("select * from employees where employee_id=?",(employee_id,))
+    conn.commit()
+    cursor.execute("select * from employees where employee_id=?",(employee_id,))
+    existing=cursor.fetchone()
+    if not existing:
+        conn.close()
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Employee not found")
+    if employee.salary<existing["salary"]:
+        conn.close()
+        raise HTTPException(status_code=400,detail="Salary cannout decrease")
     cursor.execute("""update employees set full_name=?,job_title=?,country=?,salary=? where employee_id=?""",
                    (employee.full_name,employee.job_title,employee.country,employee.salary,employee_id))
     conn.commit()
     cursor.execute("select * from employees where employee_id=?",(employee_id,))
     updated=cursor.fetchone()
     conn.close()
-    if not updated:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Employee not found")
-    return {"employee_id":updated[0],
-            "full_name":updated[1],
-            "job_title":updated[2],
-            "country":updated[3],
-            "salary":updated[4]}
+    return EmployeeResponse(employee_id=updated["employee_id"],
+                            full_name=updated["full_name"],
+                            job_title=updated["job_title"],
+                            country=updated["country"],
+                            salary=updated["salary"])
+
 
 @app.delete("/employees/{employee_id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_employee(employee_id:int):

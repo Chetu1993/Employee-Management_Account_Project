011078b848802d8363c4b341452d3e4eda8ee275
chetu1993 <schetankumar123@gmail.com>
2026-02-27 14:48:26 +0530

    test: test failed because not implemented put method

--- tests/test_management_crud.py
+++ tests/test_management_crud.py
@@ -17,3 +17,15 @@ def test_get_employee(client,created_employee_id):
     data=response.json()
     assert data["employee_id"]==emp_id
     assert data["full_name"] == "chetan kumar"
+
+def test_update_employee(client):
+    update_response=client.put("/employees",json={"full_name":"chetan",
+                                           "job_title":"frontend engineer",
+                                           "country":"India",
+                                           "salary":50000})
+
+    assert update_response.status_code == 200
+    data=update_response.json()
+    assert data["full_name"] == "chetan"
+    assert data["job_title"] == "frontend engineer"
+    assert data["country"] == "India"

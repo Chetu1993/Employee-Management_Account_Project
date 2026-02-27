55405309e8945f5f5f821265cf159aa6b31bccb9
chetu1993 <schetankumar123@gmail.com>
2026-02-27 20:33:54 +0530

    test: testing on reducing salary

--- tests/test_salary_calculation.py
+++ tests/test_salary_calculation.py
@@ -7,3 +7,21 @@ def test_salary_must_be_positive(client):
     })
 
     assert response.status_code == 422
+
+
+def test_salary_cannot_decrease_on_update(client):
+    create_response=client.post("/employees",json={
+        "full_name":"chetan kumar",
+        "job_title":"Tester",
+        "country":"India",
+        "salary":50000
+    })
+    employee_id=create_response.json()["employee_id"]
+    update_response = client.put(f"/employees/{employee_id}",json={
+        "full_name":"chetan kumar",
+        "job_title":"Tester",
+        "country":"India",
+        "salary":30000
+    })
+
+    assert update_response.status_code == 400
